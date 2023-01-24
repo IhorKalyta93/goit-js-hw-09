17056bce967d40f19fa7fcdad1806fb867c49cf5
Ihor Kalyta <ihor.kalyta93@gmail.com>
2023-01-24 15:51:03 +0100

    fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startChangeColorBtnEl = document.querySelector('[data-start]');
+const stopChangeColorBtnEl = document.querySelector('[data-stop]');
 
+startChangeColorBtnEl.addEventListener('click', onClickStartChangeColorBtnEl);
+stopChangeColorBtnEl.addEventListener('click', onStopChangeColorBtnEl);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let intervalId = null;
+
+function onClickStartChangeColorBtnEl() {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startChangeColorBtnEl.disabled = true;
+  stopChangeColorBtnEl.disabled = false;
+}
+
+function onStopChangeColorBtnEl() {
+  clearInterval(intervalId);
+  startChangeColorBtnEl.disabled = false;
+  stopChangeColorBtnEl.disabled = true;
+}
